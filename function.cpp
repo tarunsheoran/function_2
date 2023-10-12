935f493333794a3a6fad79b086d2ec36698b9bc9
Tarun Sheoran <118544285+tarunsheoran@users.noreply.github.com>
2023-10-12 23:13:11 +0530

    Add files via upload

---
+++ function.cpp
@@ -0,0 +1,38 @@
+# include <iostream>
+using namespace std;
+
+void prime();
+
+int main()
+{
+    prime();
+    return 0;
+}
+
+
+void prime()
+{
+
+    int num, i, flag = 0;
+
+    cout << "Enter a positive integer enter to check: ";
+    cin >> num;
+
+    for(i = 2; i <= num/2; ++i)
+    {
+        if(num % i == 0)
+        {
+            flag = 1; 
+            break;
+        }
+    }
+
+    if (flag == 1)
+    {
+        cout << num << " is not a prime number.";
+    }
+    else
+    {
+        cout << num << " is a prime number.";
+    }
+}
